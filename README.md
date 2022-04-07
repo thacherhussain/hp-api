4e88eb01ae13d3a9f55508ab8ed1e0c5da6ca017
Thacher Hussain <thacher.hussain@gmail.com>
2022-04-07 11:58:22 -0400

    server update

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Harry Potter API
 
-A simple JSON-based API for getting information on Harry Potter characters. 
+A simple JSON-based API for getting information on Harry Potter characters.
 
-Hosted at https://hp-api.herokuapp.com/
+Hosted at http://potterverse-v2.herokuapp.com
 
+Project forked from the [hp-api](https://github.com/bethfraser/hp-api) repo because this data file is hella complete and way more comprehensive than the one when I wrote with my first potterverse API. It's more fun to learn new things if you have an API where you can manipulate the calls as needed and my last one was waaaaaay out of date.
+
+[] change image urls
+[] update JSON file to have IDs
--- server.js
+++ server.js
@@ -1,70 +1,87 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var fs = require('fs');
-var CHARACTERS_JSON = path.join(__dirname, 'data/characters.json');
+var express = require('express')
+var app = express()
+var path = require('path')
+var fs = require('fs')
+const { json } = require('express')
+var CHARACTERS_JSON = path.join(__dirname, 'data/characters.json')
 
-app.get('/', function(req,res) {
-  res.sendfile('public/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendfile('public/index.html')
+})
 
-app.get('/api/characters', function(req, res){
-  fs.readFile(CHARACTERS_JSON, function(err, data){
-    if(err) process.exit(1);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(JSON.parse(data));
+app.get('/api/characters', function (req, res) {
+  fs.readFile(CHARACTERS_JSON, function (err, data) {
+    if (err) process.exit(1)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.json(JSON.parse(data))
   })
 })
 
-app.get('/api/characters/students', function(req, res){
-  fs.readFile(CHARACTERS_JSON, function(err, data){
-    if(err) process.exit(1);
-    json = JSON.parse(data);
-    students_array = [];
-    for(character of json){
-      if(character.hogwartsStudent == true){
-      students_array.push(character);
+app.get('/api/characters/students', function (req, res) {
+  fs.readFile(CHARACTERS_JSON, function (err, data) {
+    if (err) process.exit(1)
+    json = JSON.parse(data)
+    students_array = []
+    for (character of json) {
+      if (character.hogwartsStudent == true) {
+        students_array.push(character)
       }
     }
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(students_array);
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.json(students_array)
   })
 })
 
-app.get('/api/characters/staff', function(req, res){
-  fs.readFile(CHARACTERS_JSON, function(err, data){
-    if(err) process.exit(1);
-    json = JSON.parse(data);
-    staff_array = [];
-    for(character of json){
-      if(character.hogwartsStaff == true){
-      staff_array.push(character);
+app.get('/api/characters/staff', function (req, res) {
+  fs.readFile(CHARACTERS_JSON, function (err, data) {
+    if (err) process.exit(1)
+    json = JSON.parse(data)
+    staff_array = []
+    for (character of json) {
+      if (character.hogwartsStaff == true) {
+        staff_array.push(character)
       }
     }
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(staff_array);
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.json(staff_array)
   })
 })
 
-app.get('/api/characters/house/:house', function(req, res){
-  fs.readFile(CHARACTERS_JSON, function(err, data){
-    if(err) process.exit(1);
-    json = JSON.parse(data);
-    house_array = [];
-    for(character of json){
-      if(character.house.toLowerCase() == req.params.house.toLowerCase()){
-      house_array.push(character);
+app.get('/api/characters/house/:house', function (req, res) {
+  fs.readFile(CHARACTERS_JSON, function (err, data) {
+    if (err) process.exit(1)
+    json = JSON.parse(data)
+    house_array = []
+    for (character of json) {
+      if (character.house.toLowerCase() == req.params.house.toLowerCase()) {
+        house_array.push(character)
       }
     }
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(house_array);
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.json(house_array)
   })
 })
 
-app.use(express.static('public'));
+app.get('/api/characters/:id', function (req, res) {
+  fs.readFile(CHARACTERS_JSON, function (err, data) {
+    if (err) process.exit(1)
+    const json = JSON.parse(data)
+    character = {}
+    for (character of json) {
+      if (character.id === req.params.id) {
+        character = character
+      }
+    }
 
-app.set('port', (process.env.PORT || 5000));
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.json(character)
+  })
+})
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.use(express.static('public'))
+
+app.set('port', process.env.PORT || 5000)
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
